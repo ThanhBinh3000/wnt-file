a1ba4447eaeb1c63f9ba6d2db6a7ae0926204197
giangnt <giangnt@tecapro.com.vn>
2024-04-26 10:26:48 +0700

    update

---
+++ src/main/java/com/wnt/file/exception/QueryValidationException.java
@@ -0,0 +1,8 @@
+package com.wnt.file.exception;
+
+public class QueryValidationException extends Exception {
+
+    public QueryValidationException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/wnt/file/exception/RestTemplateException.java
@@ -0,0 +1,9 @@
+package com.wnt.file.exception;
+
+import java.io.IOException;
+
+public class RestTemplateException extends IOException {
+    public RestTemplateException(String message) {
+        super(message);
+    }
+}
